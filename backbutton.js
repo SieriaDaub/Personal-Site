9cefd7a8f6ea36de83feee26699b80fdd893935f
Sieria Daub <sieriaj1234@gmail.com>
2022-10-04 19:53:26 -0500

    this is my personal website files

---
+++ backbutton.js
@@ -0,0 +1,9 @@
+
+let button = document.querySelector('#backButton');
+
+button.addEventListener("click", function behind() {
+    window.history.back()
+})
+
+
+
---
+++ dom.js
@@ -0,0 +1,6 @@
+let portfolio = ['about','resume','contact'];
+console.log(portfolio);
+
+for (let e = 0; e < 5; e++) {
+    console.log('Hello Everyone');
+}
