778b2c87501fb0242274fec63aa2fac3feecf84a
Sieria Daub <sieriaj1234@gmail.com>
2022-10-06 12:21:42 -0500

    changes in the inner HTML

--- dom.js
+++ dom.js
@@ -1,5 +1,8 @@
-let updated = document.getElementsByClassName('learning')
-updated.innerHTML += '<p>[about,resume,contact]</p>'
+
+let updated = document.querySelector('#learning')
+updated.innerHTML = '<p> This is an example of changing the inner HTML </p>'
+
+
 
 for (let e = 0; e < 5; e++) {
     console.log('Hello Everyone');
