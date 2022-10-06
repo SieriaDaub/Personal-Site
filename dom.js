7ed26036651309c39b458d0e644b9356dbe977b7
Sieria Daub <sieriaj1234@gmail.com>
2022-10-05 21:24:03 -0500

    added more js

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
-let portfolio = ['about','resume','contact'];
-console.log(portfolio);
+let updated = document.getElementsByClassName('learning')
+updated.innerHTML += '<p>[about,resume,contact]</p>'
 
 for (let e = 0; e < 5; e++) {
     console.log('Hello Everyone');
